a7678319e05231e1acae39e8940053b7b0fe2daf
Ahmed Elbanna <140323819+AhmedElbanna098@users.noreply.github.com>
2024-05-14 21:04:27 +0300

    Add project files.

---
+++ Tests/BMWTests.cs
@@ -0,0 +1,49 @@
+﻿using CarFactoryLibrary.library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BannaCarTesting.Tests
+{
+    public class BMWTests
+    {
+        [Fact]
+        public void TestIsStoppedWhenVelocityIsZero()
+        {
+            //Arrange
+            var bmwCar = new BMW() { velocity = 0 };
+            //Act
+            bool isStopped = bmwCar.IsStopped();
+            //Assert
+            Assert.True(isStopped);
+        }
+        [Fact]
+        public void TestIsStoppedWhenVelocityIsGreaterThanZero()
+        {
+            //Arrange
+            var bmwCar = new BMW() { velocity = 50 };
+            //Act
+            bool isStopped = bmwCar.IsStopped();
+            //Assert
+            Assert.False(isStopped);
+        }
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(0, 50)]
+        [InlineData(50, -20)]
+        [InlineData(50, 30)]
+        public void TestIncreaseVelocityBmw(double initialVelocity, double addedVelocity)
+        {
+            //Arrange
+            var bmwcar = new BMW() { velocity = initialVelocity };
+            //Act
+            bmwcar.IncreaseVelocity(addedVelocity);
+            //Assert
+            double expectedVelocity = initialVelocity + addedVelocity;
+            Assert.Equal(expectedVelocity, bmwcar.velocity);
+        }
+    }
+}
---
+++ library/BMW.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFactoryLibrary.library
+{
+    public class BMW : Car
+    {
+        public override void Accelerate()
+        {
+            velocity += 15;
+        }
+        public BMW()
+        {
+            CarType = CarTypes.BMW;
+        }
+
+    }
+}
